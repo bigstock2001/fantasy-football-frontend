ade4229533d9c9f3e59c90f27815d64e064765ee
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-06 20:43:59 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
-export default function Home() { return <div>Welcome to the Locker Room</div>; }
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to the Locker Room</h1>
+      <ul>
+        <li><Link href="/login">Login</Link></li>
+        <li><Link href="/stats">Stats</Link></li>
+        <li><Link href="/trophies">Trophies</Link></li>
+      </ul>
+    </div>
+  );
+}
