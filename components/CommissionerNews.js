c1ce806626c5123b20ed3a15eb088168239c6ab9
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-13 15:22:30 -0500

    removed commisioner news

--- components/CommissionerNews.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-// Return empty by default; your banner will say “No commissioner news right now.”
-export async function GET() {
-  return NextResponse.json({}, {
-    headers: { "cache-control": "s-maxage=30, stale-while-revalidate=300" },
-  });
-}
