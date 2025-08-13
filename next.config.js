5dc78d3e94b85ffe81a95f48ac9b76cc098849d1
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-12 20:58:19 -0500

    next.config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        // Proxy all backend calls through /bff to avoid CORS issues
         source: "/bff/:path*",
         destination: "https://backend.footballforeverdynasty.us/:path*",
       },
