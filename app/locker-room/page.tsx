fcb710daeb60467a1794ca2cf75205e69dda1343
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-31 11:13:36 -0500

    update

--- app/locker-room/page.tsx
+++ app/locker-room/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function LockerRoomRedirect() {
-  redirect("https://www74.myfantasyleague.com/2025/options?L=61408&O=07");
+  redirect("https://www45.myfantasyleague.com/2025/options?L=61408&O=07");
 }
