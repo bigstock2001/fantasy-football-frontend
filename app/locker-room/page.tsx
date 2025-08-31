433bfb6452b3d906337f41be87abf51dbe858fe9
bigstock2001 <donald.l.dunn@gmail.com>
2025-08-31 11:10:53 -0500

    updae

--- app/locker-room/page.tsx
+++ app/locker-room/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function LockerRoomRedirect() {
-  // Replace with your actual league’s MFL Locker Room URL
   redirect("https://www74.myfantasyleague.com/2025/options?L=61408&O=07");
 }
